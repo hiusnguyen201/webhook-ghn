1ffe6b4553a576e95c788c0ceec15fac6b6198b9
hiusnguyen201 <hiusnguyen201@gmail.com>
2025-04-22 21:28:08 +0700

    a

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ app.post('/ghn/webhook/status', (req, res, next) => {
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  res.status(404).send('Not Found');
+  res.status(400).json({
+    message: 'Not Found',
+  });
 });
 
 // error handler
